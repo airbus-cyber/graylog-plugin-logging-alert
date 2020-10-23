c3e97bb56170d42a12e0598c34ab444c79005a2b
tomasnk <thomas.oudin@gmail.com>
2020-10-23 17:37:41 +0200

    add Copyright header

--- src/main/java/com/airbus_cyber_security/graylog/events/notifications/types/LoggingAlert.java
+++ src/main/java/com/airbus_cyber_security/graylog/events/notifications/types/LoggingAlert.java
@@ -1,3 +1,23 @@
+/*
+ * graylog-plugin-logging-alert Source Code
+ * Copyright (C) 2018-2020 - Airbus CyberSecurity (SAS) - All rights reserved
+ *
+ * This file is part of the graylog-plugin-logging-alert GPL Source Code.
+ *
+ * graylog-plugin-logging-alert Source Code is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.airbus_cyber_security.graylog.events.notifications.types;
 
 import java.util.LinkedHashSet;
