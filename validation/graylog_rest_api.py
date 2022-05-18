578c6637e6825f13ebeb755b23d9ced855f47d54
c8y3 <25362953+c8y3@users.noreply.github.com>
2022-05-18 09:06:42 +0200

    Using requests session

--- validation/graylog_rest_api.py
+++ validation/graylog_rest_api.py
@@ -8,23 +8,28 @@ _HEADERS = {'X-Requested-By': 'test-program'}
 
 class GraylogRestApi:
 
+    def __init__(self):
+        self._session = requests.Session()
+        self._session.auth = _AUTH
+        self._session.headers.update(_HEADERS)
+
     def _build_url(self, path):
         return parse.urljoin('http://127.0.0.1:9000/api/', path)
 
     def get(self, path):
         url = self._build_url(path)
-        response = requests.get(url, auth=_AUTH, headers=_HEADERS)
+        response = self._session.get(url)
         print('GET {} => {}'.format(url, response.status_code))
         return response
 
     def put(self, path, payload):
         url = self._build_url(path)
-        response = requests.put(url, json=payload, auth=_AUTH, headers=_HEADERS)
+        response = self._session.put(url, json=payload)
         print('PUT {} {} => {}'.format(url, payload, response.status_code))
         return response
 
     def post(self, path, payload=None):
         url = self._build_url(path)
-        response = requests.post(url, json=payload, auth=_AUTH, headers=_HEADERS)
+        response = self._session.post(url, json=payload)
         print('POST {} {} => {}'.format(url, payload, response.status_code))
         return response
