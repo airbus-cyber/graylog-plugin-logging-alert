38e2f028b43e925533d48ec7827ac18b5eda994a
c8y3 <25362953+c8y3@users.noreply.github.com>
2022-09-14 10:36:54 +0200

    Added a test to understand graylog MoreSearch behavior

---
+++ src/test/java/org/graylog/events/search/MoreSearchTest.java
@@ -0,0 +1,42 @@
+package org.graylog.events.search;
+
+import org.graylog.events.processor.EventProcessorException;
+import org.graylog.events.search.MoreSearch;
+import org.graylog.plugins.views.search.Parameter;
+import org.graylog.plugins.views.search.elasticsearch.QueryStringDecorators;
+import org.graylog2.indexer.ranges.IndexRangeService;
+import org.graylog2.plugin.indexer.searches.timeranges.InvalidRangeParametersException;
+import org.graylog2.plugin.indexer.searches.timeranges.RelativeRange;
+import org.graylog2.plugin.indexer.searches.timeranges.TimeRange;
+import org.graylog2.streams.StreamService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class MoreSearchTest {
+
+    private MoreSearch subject;
+
+    @Before
+    public void setup() {
+        StreamService streamService = Mockito.mock(StreamService.class);
+        IndexRangeService indexRangeService = Mockito.mock(IndexRangeService.class);
+        QueryStringDecorators esQueryDecorators = Mockito.mock(QueryStringDecorators.class);
+        MoreSearchAdapter moreSearchAdapter = Mockito.mock(MoreSearchAdapter.class);
+        this.subject = new MoreSearch(streamService, indexRangeService, esQueryDecorators, moreSearchAdapter);
+    }
+
+    @Test
+    public void scrollQueryShouldNotFail() throws InvalidRangeParametersException, EventProcessorException {
+        String query = "x: \"hello\"world\"";
+        Set<String> streams = new HashSet<>();
+        Set<Parameter> parameters = new HashSet<>();
+        TimeRange timeRange = RelativeRange.create(10);
+        MoreSearch.ScrollCallback callback = (messages, continueScrolling) -> {};
+        this.subject.scrollQuery(query, streams, parameters, timeRange, 1, callback);
+    }
+}
