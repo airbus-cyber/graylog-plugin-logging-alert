46d85452f6c1cb1be89ac883f7b698f9d14cea93
c8y3 <25362953+c8y3@users.noreply.github.com>
2023-12-08 13:57:58 +0100

    Put the click on close before

--- end_to_end/tests/end_to_end.spec.js
+++ end_to_end/tests/end_to_end.spec.js
@@ -7,12 +7,12 @@ test.beforeEach(async ({ page }) => {
   await page.getByLabel('Username').fill('admin');
   await page.getByLabel('Password').fill('admin');
   await page.getByRole('button', { name: 'Sign in' }).click();
+
+  // TODO there will most probably be a problem here on the second test: this should only be done once. Find a way to set this state once and for all before (maybe there is some REST API for that...)
+  await page.getByText('×Close').click();
 });
 
 test('Plugin Logging Alert should be registered (issue #50)', async ({ page }) => {
-  // TODO there is a problem here: this should only be done once
-  await page.getByText('×Close').click();
-
   await page.getByRole('button', { name: 'System' }).click();
   await page.getByRole('menuitem', { name: 'Configurations' }).click();
   await page.getByRole('button', { name: 'Plugins' }).click();
