240b80741a4f4dfb5b7d2917796e0f7fad6ddeb4
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-01-31 13:35:22 +0100

    Added two more interface tests

--- validation/end_to_end/test_end_to_end.py
+++ validation/end_to_end/test_end_to_end.py
@@ -32,13 +32,13 @@ def test_plugin_logging_alert_should_be_registered_issue_50(page: Page):
     # TODO should really by Logging Alert, but this will only be possible once this fix https://github.com/Graylog2/graylog2-server/issues/15939 is released
     expect(page.get_by_role('button', name='Logging')).to_have_text('com.airbus_cyber_security.graylog.events.config.LoggingAlertConfig')
 
-def test_plugin_logging_alert_configuration_save_button_should_close_popup_50(page: Page):
+def test_plugin_logging_alert_configuration_save_button_should_close_popup_issue_50(page: Page):
     _go_to_plugin_configuration(page)
     page.get_by_role('button', name='Edit configuration').click()
     page.get_by_text('Save').click()
     expect(page.get_by_text('Update Logging Alert Notification Configuration')).not_to_be_attached()
 
-def test_plugin_logging_alert_configuration_save_button_update_should_not_fail_50(page: Page):
+def test_plugin_logging_alert_configuration_save_button_update_should_not_fail_issue_50(page: Page):
     _go_to_plugin_configuration(page)
     page.get_by_role('button', name='Edit configuration').click()
     # note: we could also have done something with page.on('response', lambda response: print('<<', response.status, response.url, response.request.method, response.ok))
@@ -46,7 +46,7 @@ def test_plugin_logging_alert_configuration_save_button_update_should_not_fail_5
         page.get_by_text('Save').click()
         assert event.value.ok
 
-def test_plugin_logging_alert_configuration_cancel_button_should_revert_changes(page: Page):
+def test_plugin_logging_alert_configuration_cancel_button_should_revert_changes_issue_50(page: Page):
     _go_to_plugin_configuration(page)
     page.get_by_role('button', name='Edit configuration').click()
     page.get_by_label('Line Break Substitution').fill('+')
@@ -54,5 +54,14 @@ def test_plugin_logging_alert_configuration_cancel_button_should_revert_changes(
     page.get_by_role('button', name='Edit configuration').click()
     expect(page.get_by_label('Line Break Substitution')).to_have_value(' | ')
 
+def test_plugin_logging_alert_configuration_cancel_button_should_close_popup_issue_50(page: Page):
+    _go_to_plugin_configuration(page)
+    page.get_by_role('button', name='Edit configuration').click()
+    page.get_by_text('Cancel').click()
+    expect(page.get_by_text('Update Logging Alert Notification Configuration')).not_to_be_attached()
 
-#    page.get_by_text("×Close").click()
+def test_plugin_logging_alert_configuration_window_close_should_close_popup_issue_50(page: Page):
+    _go_to_plugin_configuration(page)
+    page.get_by_role('button', name='Edit configuration').click()
+    page.get_by_text("×Close").click()
+    expect(page.get_by_text('Update Logging Alert Notification Configuration')).not_to_be_attached()
--- validation/manual.md
+++ validation/manual.md
@@ -1,19 +1,1 @@
 # Manual scenarios which should be made automatic
-
-## Configuration
-
-* start from http://127.0.0.1:9000/system/configurations
-
-### Cancel button should close popup
-* select Plugins section
-* select Logging Alert configuration
-* click button edit configuration
-* click button Cancel
-* it should close the popup
-
-### Exit cross should close popup
-* select Plugins section
-* select Logging Alert configuration
-* click button edit configuration
-* click button exit cross
-* it should close the popup
